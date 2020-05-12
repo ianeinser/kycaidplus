9eaf5cd062475f7945802703fefd2fc9043d8ca1
Yehor Chumak <ychumak@heliostech.fr>
2020-05-12 11:54:07 +0300

    Add address and verification API support

--- lib/kycaid/address.rb
+++ lib/kycaid/address.rb
@@ -1,4 +1,22 @@
 module KYCAID
-	module Address
+  module Address
+    extend Client
+
+    def self.create(params)
+      protected_params = params.slice(:type, :country, :state_or_province, :city, :postal_code, :street_name, :building_number)
+      response = post("/addresses", protected_params)
+      new(JSON.parse(response.body))
+    end
+
+    def self.fetch(params)
+      protected_params = params.slice(:address_id, :country, :state_or_province, :city, :postal_code, :street_name, :building_number)
+      response = patch("/addresses/#{address_id}", protected_params)
+      new(JSON.parse(response.body))
+    end
+
+    def initialize(response)
+      super(response)
+      self.raw_response = response
+    end
 	end
 end
--- lib/kycaid/verification.rb
+++ lib/kycaid/verification.rb
@@ -1,4 +1,21 @@
 module KYCAID
-	module Verification
+  module Verification
+    extend Client
+
+    def self.create(params)
+      protected_params = params.slice(:applicant_id, :types, :callback_url)
+      response = post("/verifications", protected_params)
+      new(JSON.parse(response.body))
+    end
+
+    def self.fetch(applicant_id)
+      response = get("/verifications/#{verification_id}")
+      new(JSON.parse(response.body))
+    end
+
+    def initialize(response)
+      super(response)
+      self.raw_response = response
+    end
 	end
 end
