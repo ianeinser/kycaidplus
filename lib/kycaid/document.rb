5ef3fef7935c60e6908fbebcc17b416298085867
Yehor Chumak <ychumak@heliostech.fr>
2020-05-12 11:52:46 +0300

    Add file and document API support

--- lib/kycaid/document.rb
+++ lib/kycaid/document.rb
@@ -1,4 +1,22 @@
 module KYCAID
-	module Document
+  module Document
+    extend Client
+
+    def self.create(params)
+      protected_params = params.slice(:applicant_id, :type, :document_number, :issue_date, :expiry_date, :front_side_id, :back_side_id)
+      response = post("/documents", protected_params)
+      new(JSON.parse(response.body))
+    end
+
+    def self.patch(applicant_id)
+      protected_params = params.slice(:document_id, :applicant_id, :type, :document_number, :issue_date, :expiry_date, :front_side_id, :back_side_id)
+      response = get("/documents/#{document_id}", protected_params)
+      new(JSON.parse(response.body))
+    end
+
+    def initialize(response)
+      super(response)
+      self.raw_response = response
+    end
 	end
 end
--- lib/kycaid/file.rb
+++ lib/kycaid/file.rb
@@ -1,4 +1,20 @@
 module KYCAID
-	module File
+  module File
+    extend Client
+
+    def self.create(params)
+      response = file_post("/files", params.slice(:file))
+      new(JSON.parse(response.body))
+    end
+
+    def self.update(params)
+      response = file_put("/files/#{params[:file_id]}", params.slice(:file, file_id).merge(sandbox: KYCAID.configuration.sandbox_mode))
+      new(JSON.parse(response.body))
+    end
+
+    def initialize(response)
+      super(response)
+      self.raw_response = response
+    end
 	end
 end
